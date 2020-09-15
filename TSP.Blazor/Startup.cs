4870bb302898504b360e939e25e8188c1deabfd2
Arthur Lu <arthur.lv6@gmail.com>
2020-09-15 13:32:52 +1200

    options.SignIn.RequireConfirmedAccount = false

--- TSP.Blazor/Startup.cs
+++ TSP.Blazor/Startup.cs
@@ -34,7 +34,7 @@ namespace TSP.Blazor
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddRazorPages();
             services.AddServerSideBlazor();
